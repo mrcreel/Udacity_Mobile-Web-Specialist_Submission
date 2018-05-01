0f5629246ac1d3a0e9f78cdd0cca8939bef4cf30
mrcreel <michaelrcreel@gmail.com>
2018-05-01 10:30:55 -0500

    Added <picture> & <figure> tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,24 @@ module.exports = function(grunt) {
           sizes: [{
             /*
             Change these:
-            
+
             width: ,
             suffix: ,
             quality:
             */
-          }]
+           /*
+            width: '30%',
+            suffix: '_small',
+            quality: 20,
+          },
+          {
+            name: 'large',
+            width: '50%',
+            suffix: '_large',
+            quality: 40,
+            */
+          },
+          ]
         },
 
         /*
@@ -64,7 +76,7 @@ module.exports = function(grunt) {
       },
     },
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
